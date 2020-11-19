a2e0abe1fcbb7e2709890f209e1b12003b3bf187
Chad Naylor <chad.naylor@gmail.com>
2020-11-19 16:04:26 -0600

    Game works. Computer always wins.

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 const yargs = require('yargs/yargs')
 const { hideBin } = require('yargs/helpers')
 const argv = yargs(hideBin(process.argv)).argv
+
+let move = argv.move
+console.log('Playing a game of Roshambo against the computer.')
+console.log(`Player plays ${move}!`)
+
+if (move !== 'rock' && move !== 'paper' && move !== 'scissors') {
+    console.log(`INVALID MOVE. You played ${move}. Must play rock, paper, or scissors.`)
+} else if (move === 'rock') {
+    console.log(`Computer plays paper!`)
+} else if (move === 'paper') {
+    console.log(`Computer plays scissors!`)
+} else if (move === 'scissors') {
+    console.log(`Computer plays rock!`)
+}
+
+console.log(`~Computer wins.~`)

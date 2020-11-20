31afef05a1d8cc5923a150f4ccbc5ae2cbdaefea
Chad Naylor <chad.naylor@gmail.com>
2020-11-19 22:48:21 -0600

    make pretty

--- main.js
+++ main.js
@@ -2,10 +2,8 @@ const yargs = require('yargs/yargs')
 const { hideBin } = require('yargs/helpers')
 const argv = yargs(hideBin(process.argv)).argv
 
-
 class computerPlayer {
     constructor() {
-
     }
 
     generateMove() {
